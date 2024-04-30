0c91182e4ce75c5b556b8ab46bc0e4563a99720e
Randy Kwalar <rkwalar@gmail.com>
2024-04-30 18:40:41 +0100

    Initial Commit

---
+++ FrameworksApp/FrameworksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameworksAppApp.swift
+//  FrameworksApp
+//
+//  Created by Randy Kwalar on 30/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct FrameworksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
